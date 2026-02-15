90cd716dc9ce09e98a8eeb91a00c76fdec339f2e
Sulav-dkl <sulav0424@student.ku.edu.np>
2026-02-15 20:21:49 +0545

    QN number 3

---
+++ prog_3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void reversePrint(struct Node *head){
+    if(!head) return;
+    reversePrint(head->next);
+    printf("%d ", head->data);
+}
+
+int main(){
+    struct Node *head=NULL, *temp;
+    for(int i=1;i<=5;i++){
+        temp=malloc(sizeof(struct Node));
+        temp->data=i;
+        temp->next=head;
+        head=temp;
+    }
+
+    printf("Reverse Traversal: ");
+    reversePrint(head);
+
+    return 0;
+}
